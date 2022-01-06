cf7b205d0178825f3949cee938893aef17972684
RxPro <wxweb@mail.ru>
2022-01-06 16:42:02 +0300

    Редизайн проекта, буду делать все по новой

--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -20,18 +20,52 @@ $(document).ready(function(){
             $('body').removeClass('no-scroll');
             $('.menu').removeClass('menu--active')
         });
-    
-        $('.menu__link-genres').on('click', function(e) {
+
+        $('.menu__link-arrow').on('click', function(e) {
             e.preventDefault();
-            $(this).toggleClass('menu__link-genres--active');
-            $(this).siblings('.menu__submenu').toggleClass('menu__submenu--active')
+            $(this).toggleClass('menu__link-arrow--active');
+            $(this).parent('.menu__box').next('.menu__submenu').toggleClass('menu__submenu--active');
+        });
+
+        $('.menu__link-arrow--main').on('click', function(e) {
+            $('.menu__link-genres').toggleClass('menu__link-genres--active')
         });
     
-        $('.menu__submenu-item .menu__link').on('click', function(e) {
-            e.preventDefault();
-            $(this).toggleClass('menu__link--active');
-            $(this).siblings('.menu__submenu-item__submenu').toggleClass('menu__submenu-item__submenu--active')
-        });
+        // $('.menu__link-arrow').on('click', function(e) {
+        //     e.preventDefault();
+        //     $(this).toggleClass('menu__link-genres--active');
+        //     $(this).siblings('.menu__submenu').toggleClass('menu__submenu--active')
+        // });
+    
+        // $('.menu__submenu-item .menu__link').on('click', function(e) {
+        //     e.preventDefault();
+        //     $(this).toggleClass('menu__link--active');
+        //     $(this).siblings('.menu__submenu-item__submenu').toggleClass('menu__submenu-item__submenu--active')
+        // });
+    }
+
+    if($(window).width() >= 1280) {
+        // $('.menu__box-1').on('mouseenter', function(e) {
+        //     e.preventDefault();
+        //     $('.menu__submenu').removeClass('menu__submenu--active');
+        //     $(this).next('.menu__submenu').addClass('menu__submenu--active');
+        // });
+
+        // $('.menu__submenu .menu__box').on('mouseenter', function(e) {
+        //     e.preventDefault();
+        //     $(this).next('.menu__submenu').addClass('menu__submenu--active');
+        // });
+
+        // $('.menu__submenu .menu__box').on('mouseleave', function(e) {
+        //     e.preventDefault();
+        //     $(this).next('.menu__submenu').removeClass('menu__submenu--active');
+        // });
+
+        // $('.menu__box').on('mouseleave', function(e) {
+        //     e.preventDefault();
+        //     $('.menu__submenu').removeClass('menu__submenu--active');
+        //     $(this).next('.menu__submenu').removeClass('menu__submenu--active');
+        // });
     }
 });
 
